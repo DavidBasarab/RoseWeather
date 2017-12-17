0bb3b50c3a22c33ff4db007009edea3c3eb068d6
DavidBasarab <dbasarab617@gmail.com>
2017-12-16 22:50:27 -0500

    timer changes

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-var hourly10DayUrl = 'http://api.wunderground.com/api/0b7c673843d8c122/conditions/astronomy/hourly10day/forecast10day/lang:EN/q/33.365736,-84.432969.json';
+var hourly10DayUrl = 'http://api.wunderground.com/api/0b7c673843d8c122/conditions/astronomy/hourly10day/alerts/forecast10day/lang:EN/q/33.365736,-84.432969.json';
 var googleMapImageUrl = 'http://maps.googleapis.com/maps/api/staticmap?center=33.365736,-84.432969&zoom=10&size=933x840&maptype=hybrid&markers=color:red|size:small|33.365736,-84.432969';
 var radarImageUrl = 'http://api.wunderground.com/api/0b7c673843d8c122/animatedradar/lang:EN/image.gif?maxlat=40.7209201431&maxlon=-74.1827248594&minlat=25.3324717742&minlon=-94.6832131406&width=1866&height=1680&newmaps=0&reproj.automerc=1&num=5&delay=25&timelabel=1&timelabel.y=10&rainsnow=1&smooth=1&radar_bitmap=1&xnoclutter=1&xnoclutter_mask=1&cors=1';
 
@@ -22,11 +22,13 @@ $(document).ready(function () {
         getWeatherData().done(function (result) {
             console.log('got weather data');
 
+            //"http://icons.wxug.com/i/c/k/nt_partlycloudy.gif
+
             $('#currentTemp').text(result.current_observation.temp_f);
 
             $('#currentConditions').text(result.current_observation.weather);
 
-            $('#currentConditionsIcon').attr('src', result.current_observation.icon_url);
+            $('#currentConditionsIcon').attr('src', 'https://icons.wxug.com/i/c/g/' + result.current_observation.icon + '.gif');
 
             // Need to find RAIN % for current hour
             var currentHour = result.hourly_forecast[0];
